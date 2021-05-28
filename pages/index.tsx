67f407bda4484e40339c0170f3f78ac4023e68bf
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-05-28 19:06:34 -0300

    Aula 'Validando visitante' (C4-M3-SM3)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
-import { GetServerSideProps } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { parseCookies } from 'nookies'
 import { useState, FormEvent, useContext } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest'
 
 export default function Home() {
   const [email, setEmail] = useState('')
@@ -32,24 +29,8 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const cookies = parseCookies(ctx)
-
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false
-      }
-    }
-  }
-
-  if(ctx.req.cookies) {
-  }
-
+export const getServerSideProps = withSSRGuest(async (ctx) => {
   return {
-    props: {
-      
-    }
+    props: {}
   }  
-}
+})
